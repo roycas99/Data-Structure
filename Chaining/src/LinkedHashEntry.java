3e04e316561684965323cf405bb302339636485f
roycas99 <xg2163rd@go.minnstate.edu>
2020-11-19 18:32:16 -0600

    One way to avoid collusion is chaining

---
+++ Chaining/src/LinkedHashEntry.java
@@ -0,0 +1,12 @@
+ class LinkedHashEntry {
+    String key;
+    int value;
+    LinkedHashEntry next;
+
+    // constructor
+     LinkedHashEntry(String key,int value){
+         this.key=key;
+         this.value=value;
+         this.next=null;
+     }
+}
